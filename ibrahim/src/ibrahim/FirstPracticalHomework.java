2f66d7aa55d48e0d96c10f9e42ee3ccb3e513b1b
LENOVO1 <LENOVO1@DESKTOP-98AGJ0J>
2021-02-06 10:04:25 -0800

    First commit

---
+++ ibrahim/src/ibrahim/FirstPracticalHomework.java
@@ -0,0 +1,11 @@
+package ibrahim;
+//Written by Ibrahim Al-Mahamed
+//Date:6/2/2021
+public class FirstPracticalHomework {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Ibrahim");
+	}
+
+}
